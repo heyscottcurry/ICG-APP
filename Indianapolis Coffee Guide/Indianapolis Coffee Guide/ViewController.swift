454a666e71babe73bb14f51ce416c615948a788d
Scott Curry <scurry@FusionAdmins-MacBook-Pro.local>
2017-03-04 21:09:44 -0500

    First push

---
+++ Indianapolis Coffee Guide/Indianapolis Coffee Guide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Indianapolis Coffee Guide
+//
+//  Created by Scott Curry on 3/4/17.
+//  Copyright © 2017 Scott Curry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
