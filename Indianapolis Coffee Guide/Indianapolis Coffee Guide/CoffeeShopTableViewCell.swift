658eb1e6bc8c0f3949ccebb190b37d7d323e6ae5
Scott Curry <scurry@FusionAdmins-MacBook-Pro.local>
2017-03-04 21:36:06 -0500

    no message

---
+++ Indianapolis Coffee Guide/Indianapolis Coffee Guide/CoffeeShopTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  CoffeeShopTableViewCell.swift
+//  Indianapolis Coffee Guide
+//
+//  Created by Scott Curry on 3/4/17.
+//  Copyright © 2017 Scott Curry. All rights reserved.
+//
+
+import UIKit
+
+class CoffeeShopTableViewCell: UITableViewCell {
+
+    ///MARK: Properties
+    
+    
+    @IBOutlet weak var featureThumbnail: UIImageView!
+    @IBOutlet weak var shopNeighborhood: UILabel!
+    @IBOutlet weak var shopDistance: UILabel!
+    @IBOutlet weak var shopName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
