84ccdb1e1417ae20706246f85b3ab742856e9b8a
Scott Curry <scurry@FusionAdmins-MacBook-Pro.local>
2017-03-10 10:15:28 -0500

    Firebase installed :)

---
+++ Indianapolis Coffee Guide/Pods/Target Support Files/Pods-Indianapolis Coffee Guide/Pods-Indianapolis Coffee Guide-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Indianapolis_Coffee_Guide : NSObject
+@end
+@implementation PodsDummy_Pods_Indianapolis_Coffee_Guide
+@end
---
+++ Indianapolis Coffee Guide/Pods/Target Support Files/Pods-Indianapolis Coffee GuideUITests/Pods-Indianapolis Coffee GuideUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Indianapolis_Coffee_GuideUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Indianapolis_Coffee_GuideUITests
+@end
